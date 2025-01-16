4b5c0782cba5c9bfcd88a3140055dc83d9c65660
samuel.pospisil <samuel.pospisil@tul.cz>
2025-01-16 21:36:01 +0100

    Razor pages

--- Controllers/ProductionPredictionController.cs
+++ Controllers/ProductionPredictionController.cs
@@ -13,15 +13,26 @@ namespace PredikceVytěžováníFVE.Controllers
     public class ProductionPredictionController : ControllerBase
     {
         readonly private MeteoSourceService meteoService = new();
+        readonly private OpenWeatherService weatherService = new();
+
+
+        //[HttpGet]
+        //public async void GetWeather()
+        //{
+        //    PointQuery apiPoint = new()
+        //    {
+        //        placeID = "postal-cz-46601",
+        //    };
+        //    var data = await meteoService.GetPoint(apiPoint);
+        //    Console.WriteLine(data.ToString());
+        //}
 
         [HttpGet]
         public async void GetPrediction()
         {
-            PointQuery apiPoint = new()
-            {
-                placeID = "postal-cz-46601",
-            };
-            var data = await meteoService.GetPoint(apiPoint);
+            string lat = "50.7260878";
+            string lon = "15.1675150";
+            var data = await weatherService.Call(lat, lon);
             Console.WriteLine(data.ToString());
         }
     }
