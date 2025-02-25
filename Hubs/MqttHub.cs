af23d39bd8509015c6259f485467af423970c21d
samuel.pospisil <samuel.pospisil@tul.cz>
2025-02-25 18:44:57 +0100

    working mqtthub

--- Hubs/MqttHub.cs
+++ Hubs/MqttHub.cs
@@ -1,18 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using MQTTnet;
-using PredikceVytěžováníFVE.Services;
 
 namespace PredikceVytěžováníFVE.Hubs {
     public class MqttHub : Hub {
 
-        readonly private MQTTService mqttService = new("147.230.76.38");
-
-        public async Task Subscribe() {
-            await mqttService.Connect();
-            await mqttService.Subscribe("FVE/Ibehej_TX", e => {
-                Console.WriteLine(e.ApplicationMessage.ConvertPayloadToString());
-                return Clients.All.SendAsync("ReceiveMqtt", e.ApplicationMessage.ConvertPayloadToString());
-            } );
-        }
     }
 }
